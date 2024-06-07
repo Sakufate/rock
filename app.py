6859996d76335622fa149d11836a790fffa4a8f9
Sakufate <sylwekskowron@gmail.com>
2024-06-07 03:28:07 +0200

    prepared for container

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
-import streamlit as st
-#python-dotenv
 import sys
 from dotenv import load_dotenv
-#import dotenv
-import getpass
 import os
-
+import streamlit as st
 from PyPDF2 import PdfReader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
-#from langchain.chains.history_aware_retriever import  create_history_aware_retriever
-#from langchain.chains.retrieval import create_retrieval_chain
-#from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
-#from langchain_chroma import Chroma
-#from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from htmlTemplates import css, bot_template, user_template
-#langchain_core
+
 #streamlit run app.py
-#if not os.environ.get("OPENAI_API_KEY"):
-    #os.environ["OPENAI_API_KEY"] = getpass.getpass()
 
+def get_pdfs_from_resources():
+    loader = PyPDFDirectoryLoader("resources/")
+    docs = loader.load()
+    return docs
 
-#llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+def get_docs_text(docs):
+    text = ""
+    for file in docs:
+        text += file.page_content
+    return text
 
 
 def get_pdf_text(pdf_docs):
@@ -52,7 +50,6 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-    #llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
     llm = ChatOpenAI()
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chan = ConversationalRetrievalChain.from_llm(llm = llm, retriever = vectorstore.as_retriever(), memory = memory)   
@@ -63,46 +60,47 @@ def handle_userinput(user_question):
     st.session_state.chat_history = response['chat_history']
 
     for i, message in enumerate(st.session_state.chat_history):
-        if 1 % 2 == 0:
+        if i % 2 == 0:
             st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
         else:
             st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+def update_button_clicked():
+    st.session_state.button_clicked = True
 
 def main():
     load_dotenv()
-    #OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-    #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     OPENAI_API_KEY = os.environ["OPENAI_API_KEY"] 
-    #print(os.getenv("OPENAI_API_KEY"))
-    #os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-    #if not os.environ.get("OPENAI_API_KEY"):
-    #    os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
-    
-    
 
     st.set_page_config(page_title="Scan knowledge base", page_icon=":books:")
 
     st.write(css, unsafe_allow_html=True)
 
+
+    if "data_not_ready" not in st.session_state:
+        st.session_state.data_not_ready = True
+
     if "conversation" not in st.session_state:
-        st.session_state.conversation = None
+        st.session_state.conversation = None    
 
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Scan knwoledge base :books:")
-    user_question = st.text_input("Input the question for the knowledge base")
-    if user_question:
-        handle_userinput(user_question)
-
+    if 'button_clicked' not in st.session_state:
+        st.session_state.button_clicked = False
 
     with st.sidebar:
         st.subheader("Knowledge base documents")
-        pdf_docs = st.file_uploader("Upload files", accept_multiple_files=True)
-        if st.button("Process"):
+        button = st.button("Process", on_click=update_button_clicked, disabled=st.session_state.button_clicked)
+
+        if button:
             with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+
+                # get pdf docs from resources folder
+                pdf_folder_docs = get_pdfs_from_resources()
+                
+                # get pdf docs from resources folder
+                raw_text = get_docs_text(pdf_folder_docs)
+
                 # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
                 
@@ -112,6 +110,18 @@ def main():
                 # create conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
 
+                #allow to write the question for the knowledge base
+                st.session_state.data_not_ready = not st.session_state.data_not_ready
+
+                #show that processing is Done
+                st.success('Processing Done!', icon="✅",)
+
+    st.header("Scan knwoledge base :books:")
+
+    user_question = st.text_input(label="Input the question for the knowledge base", disabled=st.session_state.data_not_ready)   
+
+    if user_question:
+        handle_userinput(user_question)    
 
 if __name__ == '__main__':
     main()
--- main.py
+++
@@ -1,7 +0,0 @@
-#python-dotenv
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print(os.getenv('MY_API_KEY'))
